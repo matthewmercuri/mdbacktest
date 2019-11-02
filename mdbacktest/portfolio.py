9ab38f1005de073f516a39a4c9a3615617ad3ff9
Matthew Mercuri <mercurimatthew@gmail.com>
2019-11-02 13:22:09 -0400

    some skeleton coding

--- mdbacktest/portfolio.py
+++ mdbacktest/portfolio.py
@@ -3,8 +3,27 @@ import pandas as pd
 
 class Portfolio:
 
-    def __init__(self, startingcash):
-        self.cash = startingcash
+    def __init__(self, starting_cash):
+        '''initializes portfolio object
 
-        position_df_columns = ['SecurityName', 'SecurityType', 'SecurityQuantity', 'UnitPurchaseValue', 'UnitMarketValue']
+        arguments:
+        startingcash (int): starting cash balance of the portfolio
+
+        returns:
+        portfolio (obj): cotnains all the positions and cash balance
+        '''
+        self.cash = starting_cash
+
+        position_df_columns = ['SecurityName', 'SecurityType',
+                               'SecurityQuantity', 'UnitPurchaseValue',
+                               'UnitMarketValue']
         self.positions = pd.DataFrame(columns=position_df_columns)
+
+    def rebalance(self, rebalance_strategy):
+        '''rebalances the portfolio
+
+        arguments:
+        self (obj): the portfolio object
+        rebalance_strategy(dict): a dictionary that lists the positions (key),
+        how much of the porfolio should be invested in each position (value)
+        '''
